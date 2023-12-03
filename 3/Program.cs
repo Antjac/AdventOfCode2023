0958260c46f4a015ea7150659ad36e99104f407e
Antoine ALEXANDRE <aalexandre@menway.com>
2023-12-03 20:25:56 +0100

    Fix

--- 3/Program.cs
+++ 3/Program.cs
@@ -18,11 +18,11 @@ foreach (string line in lines)
             if (!isNumber)
             {
                 isNumber = true;
-                lNumbers[l].Add((i+1,-1));
+                lNumbers[l].Add((i+1, i + 1));
             }
             else
             {
-                lNumbers[l][lNumbers[l].Count() - 1] = (lNumbers[l][lNumbers[l].Count() - 1].start, i);
+                lNumbers[l][lNumbers[l].Count() - 1] = (lNumbers[l][lNumbers[l].Count() - 1].start, i+1);
             }
         }
         else
@@ -30,7 +30,6 @@ foreach (string line in lines)
             if (isNumber)
             {
                 isNumber = false;
-                lNumbers[l][lNumbers[l].Count() - 1] = (lNumbers[l][lNumbers[l].Count() - 1].start, i);
             }
         }
 
@@ -47,15 +46,10 @@ foreach (var line in lNumbers)
     foreach (var numb in line.Value)
     {
         bool isGood = false;
-        var endCorrected = numb.end;
-        if (endCorrected == -1)
-        {
-            endCorrected = lines[0].Length;
-        }
         if (line.Key > 1)
         {
             //Check line above
-            if (lSpecials[line.Key-1].Any(x => x >= numb.start - 1 && x <= endCorrected + 1 ))
+            if (lSpecials[line.Key-1].Any(x => x >= numb.start - 1 && x <= numb.end + 1 ))
             {
                 isGood = true;
             }
@@ -64,7 +58,7 @@ foreach (var line in lNumbers)
         if (line.Key < lNumbers.Count())
         {
             //check line below
-            if (lSpecials[line.Key + 1].Any(x => x >= numb.start - 1 && x <= endCorrected + 1))
+            if (lSpecials[line.Key + 1].Any(x => x >= numb.start - 1 && x <= numb.end + 1))
             {
                 isGood = true;
             }
@@ -79,10 +73,10 @@ foreach (var line in lNumbers)
             }
         }
 
-        if (endCorrected < lines[0].Length)
+        if (numb.end < lines[0].Length)
         {
             //Check the right side
-            if (lSpecials[line.Key].Any(x=> x == endCorrected + 1))
+            if (lSpecials[line.Key].Any(x=> x == numb.end + 1))
             {
                 isGood = true;
             }
@@ -91,15 +85,14 @@ foreach (var line in lNumbers)
         if (isGood)
         {
             StringBuilder o = new StringBuilder();
-            for (var i = numb.start-1; i < endCorrected; i++)
+            for (var i = numb.start-1; i < numb.end; i++)
             {
                 o.Append(lines[line.Key-1][i]);
             }
+            Console.WriteLine(o);
             sum += int.Parse(o.ToString());
         }
     }
-
-    Console.WriteLine(sum);
 }
 
 Console.WriteLine(sum);
